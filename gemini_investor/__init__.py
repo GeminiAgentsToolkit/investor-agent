f8109019ed13d6a8cf623ce8dfd4d91c48da9917
Viacheslav Kovalevskyi <viacheslav@kovalevskyi.com>
2024-08-14 20:29:05 -0700

    portfolio now shows all the required dat

--- gemini_investor/__init__.py
+++ gemini_investor/__init__.py
@@ -1,12 +1,30 @@
+
+from tabulate import tabulate
 from gemini_investor.alpaca_utils import TradingClientSingleton
 from alpaca.trading.requests import MarketOrderRequest, OrderSide, TimeInForce, OrderType, LimitOrderRequest, OrderClass, GetOrdersRequest, QueryOrderStatus, StopLossRequest
 from gemini_investor.options_calls import get_option_contract
-from dateutil import parser
 
 
 def get_portfolio():
-    """Returns a string with the portfolio of the account.
-    """
-    positions = TradingClientSingleton.get_instance().get_all_positions()
-    return "\n".join([f"{pos.symbol}: {pos.qty}" for pos in positions if pos.qty != 0])
+    """Returns a formatted table representing the portfolio of the account."""
 
+    positions = TradingClientSingleton.get_instance().get_all_positions()
+
+    # Filter out positions with zero quantity and prepare data for the table
+    data = [
+        {
+            "Symbol": pos.symbol,
+            "Quantity": pos.qty,
+            "Cost Basis": pos.cost_basis,
+            "Unrealized P/L (%)": f"{round(float(pos.unrealized_plpc), 2) if pos.unrealized_plpc else 0}%",
+            "Unrealized P/L Today": pos.unrealized_intraday_pl,
+            "Current Price": pos.current_price,
+            "Market Value": pos.market_value  # Added for more context
+        }
+        for pos in positions if pos.qty != 0
+    ]
+
+    # If there are no positions, return a simple message
+    if not data:
+        return "No open positions in your portfolio."
+    return tabulate(data, headers="keys", tablefmt="grid")
