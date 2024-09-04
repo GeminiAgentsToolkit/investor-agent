bfab499d13aa2bb068ab2a793bf0fc4d8d3ed149
Viacheslav Kovalevskyi <viacheslav@kovalevskyi.com>
2024-09-04 12:14:10 -0700

    udpated investing pipeline

--- pipeline.py
+++ pipeline.py
@@ -2,6 +2,7 @@ from gemini_agents_toolkit.pipeline.eager_pipeline import EagerPipeline
 import investor_agent
 
 import vertexai
+import time
 
 
 vertexai.init(project="gemini-trading-backend", location="us-west1")
@@ -19,41 +20,31 @@ def create_client():
          on_message=on_message_received)
 
 
-investor_agent = create_client()
+def run_the_pipeline():
+    investor_agent = create_client()
 
-pipeline = EagerPipeline(investor_agent)
-if not pipeline.boolean_step("are we using paper account?"):
-    pipeline.step("switch to paper account")
-if not pipeline.boolean_step("market open now"):
+    pipeline = EagerPipeline(investor_agent)
+    if pipeline.boolean_step("is the market closed now?"):
+        return
+    if pipeline.boolean_step("are we using prod account(and not paper account)?"):
+        pipeline.step("switch to paper account")
     if pipeline.boolean_step("check if I own more or equal of 1000 shares of TQQQ"):
-        if not pipeline.boolean_step("is there a limit sell for TQQQ exists already?"):
-            pipeline.step("check current price of TQQQ")
-            pipeline.step("set limit sell order for 1000 TQQQ for price +4% of current price")
-            print("no limits sell")
-        print("market is open")
+        if pipeline.boolean_step("is there a limit sell for TQQQ exists already?"):
+            return
+        pipeline.step("check current price of TQQQ")
+        pipeline.step("set limit sell order for 1000 TQQQ for price +4% of current price")
+        return
     else:
         if pipeline.boolean_step("is there a limit buy exists already?"):
             pipeline.step("check current price of TQQQ")
-            if not pipeline.boolean_step("is there current limit buy price lower than curent price of TQQQ -5%?"):
+            if pipeline.boolean_step("is there current limit buy price lower than curent price of TQQQ -5%?"):
                 pipeline.step("cancel limit buy order")
                 pipeline.step("""set limit buy order for 1000 of TQQQ for price 3 precent below the current price. 
-                              Do not return compute formula, do compute of the price yourself in your head""")
+                            Do not return compute formula, do compute of the price yourself in your head""")
+    print(pipeline.summary())
 
-# 1000
 
-print(pipeline.summary())
-# if not pipeline.boolean_step("check if I own more than 30 shares of TQQQ"):
-#     if not pipeline.boolean_step("is there a limit sell for TQQQ exists already?"):
-#         pipeline.step("check current price of TQQQ")
-#         pipeline.step("set limit sell order for TQQQ for price +4% of current price")
-#     else:
-#         print("everywhere")
-#         if pipeline.boolean_step("is there a limit buy exists already?"):
-#             if not pipeline.boolean_step("is there current limit buy price lower than curent price of TQQQ -5%?"):
-#                 pipeline.step("cancel limit buy order")
-#                 pipeline.step("""set limit buy order for TQQQ for price 3 precent below the current price. 
-#                               Do not return compute formula, do compute of the price yourself in your head""")
-#         else:
-#             pipeline.step("""set limit buy order for TQQQ for price 3 precent below the current price. 
-#                           Do not return compute formula, do compute of the price yourself in your head""")
-# print(pipeline.summary())
+# run the pipeline once each 1 hour
+while True:
+    run_the_pipeline()  
+    time.sleep(3600)  
